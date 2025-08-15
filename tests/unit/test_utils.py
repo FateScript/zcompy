ea3c43cd555c92210ed8c72e5d78b8a79b6901cd
FateScript <wangfeng19950315@163.com>
2025-08-15 15:46:20 +0800

    chore: update zsh_completion_func in utils

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from zcompy.utils import pattern_to_glob, source_by_options_denpendency
+from zcompy.utils import pattern_to_glob, source_by_options_denpendency, zsh_completion_function
 
 
 @pytest.mark.parametrize("pattern,expected", [
@@ -29,3 +29,43 @@ def test_source_by_options_dependency(options, expected_source, expected_local_n
     src, name = source_by_options_denpendency(options)
     assert src == expected_source, f"Expected: {expected_source}, got: {src}"
     assert name == expected_local_name, f"Expected: {expected_local_name}, got: {name}"
+
+
+_F_SHELL = r"""
+f() {
+  local -a choices
+  local line opt msg
+
+  while IFS= read -r line; do
+    opt="${line%% *}"
+    msg="${line#* }"
+    choices+=("$opt:$msg")
+  done < <(__f)
+  _describe -t choices 'choices' choices
+}
+"""
+
+_F_SHELL_OPT = r"""
+f() {
+  local -a choices
+  local line opt msg
+  local a_value
+  a_value=${opt_args[-a]:-${opt_args[--full]}}
+
+  while IFS= read -r line; do
+    opt="${line%% *}"
+    msg="${line#* }"
+    choices+=("$opt:$msg")
+  done < <(__f "$a_value")
+  _describe -t choices 'choices' choices
+}
+"""
+
+
+@pytest.mark.parametrize("shell_name, command, dependencies, answer", [
+    ("f", "__f", None, _F_SHELL),
+    ("f", "__f", ("-a", "--full"), _F_SHELL_OPT),
+])
+def test_zsh_com_func(shell_name, command, dependencies, answer):
+    src = zsh_completion_function(shell_name, command, dependencies)
+    assert src.strip() == answer.strip()
--- zcompy/utils.py
+++ zcompy/utils.py
@@ -26,23 +26,14 @@ def pattern_to_glob(pattern: str | tuple[str]) -> str:
     return ""
 
 
-def zsh_completion_function(func_name: str, command: str) -> str:
-    """Generate a zsh completion function source."""
-    shell_template = """
-{func_name}() {{
-  local -a choices
-  local line opt msg
-
-  while IFS= read -r line; do
-    opt="${{line%% *}}"
-    msg="${{line#* }}"
-    choices+=("$opt:$msg")
-  done < <({command})
-  _describe -t choices 'choices' choices
-}}
-"""
-    shell_source = shell_template.format(func_name=func_name, command=command)
-    return shell_source
+def _ensure_denpendency(
+    opt: str | tuple[str, ...] | list[tuple[str, ...]]
+) -> list[tuple[str, ...]]:
+    if isinstance(opt, str):
+        opt = (opt,)
+    if not isinstance(opt[0], (tuple, list)):
+        opt = [opt]
+    return opt
 
 
 def source_by_options_denpendency(options_depend_on: str | tuple[str, ...]) -> tuple[str, str]:
@@ -66,6 +57,51 @@ def source_by_options_denpendency(options_depend_on: str | tuple[str, ...]) -> t
     return full_source, full_name
 
 
+def zsh_completion_function(
+    func_name: str,
+    command: str,
+    options_dependency: str | tuple[str, ...] | list[tuple[str, ...]] | None = None
+) -> str:
+    """Generate a zsh completion function source.
+
+    Args:
+        func_name (str): The name of the shell function.
+        command (str): The command executed to generate completions.
+        options_dependency (str | tuple[str, ...]): The options that this completion depends on.
+            Default to None, which means no dependency options are required.
+    """
+    assignments = ""
+    if options_dependency:
+        options_dependency = _ensure_denpendency(options_dependency)
+        sources, var_names = [], []
+        for opt in options_dependency:
+            src, name = source_by_options_denpendency(opt)
+            sources.append(src)
+            var_names.append(name)
+        var_declar = f"local {', '.join(var_names)}"
+        assignments = "\n".join([f"  {x}" for x in [var_declar] + sources]) + "\n"
+        var_suffix = ' '.join(f'"${name}"' for name in var_names)
+        command = f"{command} {var_suffix}"
+
+    shell_template = """
+{func_name}() {{
+  local -a choices
+  local line opt msg
+{assignments}
+  while IFS= read -r line; do
+    opt="${{line%% *}}"
+    msg="${{line#* }}"
+    choices+=("$opt:$msg")
+  done < <({command})
+  _describe -t choices 'choices' choices
+}}
+"""
+    shell_source = shell_template.format(
+        func_name=func_name, assignments=assignments, command=command
+    )
+    return shell_source
+
+
 def python_func_as_shell_source(func: Callable) -> tuple[str, str]:
     """Generate shell code that embeds a Python function.
 
@@ -75,12 +111,36 @@ def python_func_as_shell_source(func: Callable) -> tuple[str, str]:
     Returns:
         A tuple containing the shell code and the function name.
     """
+    indent = " " * 2  # shell indent
     func_name = func.__name__
     func_source = inspect.getsource(func)
-    full_source = f"{func_source}\n{func_name}()"
-    shell_code = f"""__{func_name}() {{
-    python3 -c \\
+    num_args = func.__code__.co_argcount
+    if num_args == 0:
+        full_source = f"{func_source}\n{func_name}()"
+        shell_code = f"""__{func_name}() {{
+{indent}python3 -c \\
 {shlex.quote(full_source)}
 }}
 """
+    else:  # has_args, "import sys" needed
+        args_source = ", ".join([f"sys.argv[{i}]" for i in range(1, num_args + 1)])
+        full_source = f"""
+import sys
+
+{func_source}
+
+if len(sys.argv) > {num_args}:
+    {func_name}({args_source})
+"""
+        local_assign = [f'local arg{i}_value="${i}"' for i in range(1, num_args + 1)]
+        assignment = "\n".join(indent + x for x in local_assign)
+        shell_args = " ".join(f'"$arg{i}_value"' for i in range(1, num_args + 1))
+
+        shell_code = f"""__{func_name}() {{
+{assignment}
+{indent}python3 -c \\
+{shlex.quote(full_source)} {shell_args} 2>/dev/null
+}}
+"""
+
     return shell_code, f"__{func_name}"
